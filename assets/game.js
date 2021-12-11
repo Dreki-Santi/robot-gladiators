f798a30458281bad4f43a20ab62c6e98ac8da3af
Mob <dreki.santi@protonmail.com>
2021-12-10 22:39:09 -0700

    Fixed blank/null name issue

--- assets/game.js
+++ assets/game.js
@@ -161,9 +161,21 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+//function to set player name
+var getPlayerName = function() {
+    var name = "";
+
+while(name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+}
+
+console.log("Your robot's name is " + name);
+return name;
+}
+
 //Player and enemy info
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
